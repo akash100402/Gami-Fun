e763ad83fa12e1d80d1d8fd6febfb244efe8443a
AKASH A J <akashjayanthi2019@gmail.com>
2024-03-25 00:25:29 +0530

    server setup

---
+++ api/index.js
@@ -0,0 +1,9 @@
+import { log } from 'console';
+import express from 'express';
+
+const app = express();
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running in ${PORT}`);
+})
