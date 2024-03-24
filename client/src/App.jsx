34fae4135c4e784687a4935cce9aeb82e49b8f0e
AKASH A J <akashjayanthi2019@gmail.com>
2024-03-24 21:46:30 +0530

    all dependencies added

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Explore from "./pages/Explore";
+import Header from "./components/Header";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from 'flowbite-react'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <Navbar>
+      
+    </Navbar>
+  )
+}
